ceace2c49a89829395ad7abf2b0ec83305d24153
Nestor Urquiza <nestor.urquiza@gmail.com>
2012-09-21 00:43:03 -0400

    gnome-terminal based script to open applications in different terminal tabs, especially useful for ssh connections

---
+++ common/tools/run-in-tabs.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+# name: run-in-tabs.sh
+# date: 20120921
+# author: Nestor Urquiza
+
+USAGE="Usage: `basename $0` <commandsFile>"
+
+if [ $# -ne "1" ] 
+then
+  echo $USAGE
+  exit 1 
+fi
+
+commandsFile=$1
+old_IFS=$IFS
+IFS=$'\n'
+lines=($(cat $commandsFile)) # array
+IFS=$old_IFS
+
+runInTabsCommand="gnome-terminal"
+for command in "${lines[@]}"
+do
+  if [[ $command =~ ^[^#].* ]]; then
+    runInTabsCommand="$runInTabsCommand --tab -e \"$command\""
+  fi
+done
+echo Running $runInTabsCommand
+`$runInTabsCommand`
