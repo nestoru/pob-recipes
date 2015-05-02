f17055df8f36fc58f12c821502e5ce3f22d3dad6
Nestor Urquiza <nestor.urquiza@gmail.com>
2015-05-02 12:58:46 -0400

    added nodejs recipe

---
+++ common/nodejs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -e
+# common/nodejs.sh
+common/tools/print-execution-info.sh $0 "$*"
+
+USAGE="Usage: `basename $0` <version>"
+
+if [ $# -ne "1" ] 
+then
+  echo $USAGE
+  exit 1 
+fi
+
+version=$1
+user=`logname`
+os=`uname`
+
+if [[ "$os" =~ "Darwin" ]]; then
+  distroName=node-v${version}-darwin-x64
+else
+  distroName=node-v${version}-linux-x64
+fi
+
+distroFileName=${distroName}.tar.gz
+cd /opt
+curl -O http://nodejs.org/dist/v${version}/$distroFileName
+tar -zxvf $distroFileName
+rm -f /usr/local/bin/node
+rm -f /usr/bin/node
+ln -s /opt/$distroName/bin/node /usr/bin/node
+rm -f /usr/local/bin/npm
+rm -f /usr/bin/npm
+ln -s /opt/$distroName/bin/npm /usr/bin/npm
+node --version
+mkdir -p /opt/nodejs
+rm $distroFileName
