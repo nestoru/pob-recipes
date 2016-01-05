14acde7503013c82957b662d51b31ef9563ebdfb
Nestor Urquiza <nestor.urquiza@gmail.com>
2016-01-05 15:37:06 -0500

    Update nodejs.sh

--- common/nodejs.sh
+++ common/nodejs.sh
@@ -13,25 +13,26 @@ version=$1
 user=`logname`
 os=`uname`
 
-# Only OSX and linux. Solaris binaries are not officially packaged. Open solaris is though.
+rm -f /usr/local/bin/node
+rm -f /usr/bin/node
+rm -f /usr/local/bin/npm
+rm -f /usr/bin/npm
+
 if [[ "$os" =~ "Darwin" ]]; then
   distroName=node-v${version}-darwin-x64
 else
   distroName=node-v${version}-linux-x64
 fi
 
+rm -fr /opt/node-*
 distroFileName=${distroName}.tar.gz
 cd /opt
 curl -O http://nodejs.org/dist/v${version}/$distroFileName
 tar -zxvf $distroFileName
-rm -f /usr/local/bin/node
-rm -f /usr/bin/node
-ln -s /opt/$distroName/bin/node /usr/bin/node
+mkdir -p /usr/local/bin/
 ln -s /opt/$distroName/bin/node /usr/local/bin/node
-rm -f /usr/local/bin/npm
-rm -f /usr/bin/npm
-ln -s /opt/$distroName/bin/npm /usr/bin/npm
 ln -s /opt/$distroName/bin/npm /usr/local/bin/npm
-node --version
-mkdir -p /opt/nodejs
+rm -fr /usr/local/lib/node_modules
 rm $distroFileName
+npm config set prefix /usr/local
+node --version
