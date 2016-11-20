51297d29a1dc939371b188b2563a1a128592d588
Nestor Urquiza <nestor.urquiza@gmail.com>
2016-11-20 17:56:30 -0500

    Update change-hostname.sh

--- common/change-hostname.sh
+++ common/change-hostname.sh
@@ -11,9 +11,7 @@ then
   exit 1 
 fi
 
-#sed -i "/$hostname/d" /etc/hosts && bash -c "echo '127.0.0.1 $hostname' >> /etc/hosts"
-HOST_ENTRY="${grep $hostname /etc/hosts}"
-: ${HOST_ENTRY:?"$hostname not found in /etc/hosts"}
+sed -i "/$hostname/d" /etc/hosts && bash -c "echo '127.0.0.1 $hostname' >> /etc/hosts"
 echo $hostname > /etc/hostname
 hostname -F /etc/hostname
 
