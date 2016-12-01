2a25ccf515616e564756b387eb6049431075144b
Nestor Urquiza <nestor.urquiza@gmail.com>
2016-12-01 16:25:34 -0500

    Update docker-install.sh

--- common/ubuntu/docker-install.sh
+++ common/ubuntu/docker-install.sh
@@ -1,16 +1,18 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 # docker-install.sh
 # @author Nestor Urquiza
 
 curl -sSL https://get.docker.com/ | sh
-usermod -aG docker $USER
-apt-get install -y python-pip
-pip install docker-compose
-newgrp docker
-service docker restart
-major=$(cat /etc/lsb-release | grep DISTRIB_RELEASE | sed -E 's/^[^0-9]*([0-9]*)\..*$/\1/g') 
+sudo newgrp docker
+sudo usermod -aG docker `logname`
+sudo apt-get install -y python-pip
+sudo pip install docker-compose
+sudo service docker restart
+major=$(cat /etc/lsb-release | grep DISTRIB_RELEASE | sed -E 's/^[^0-9]*([0-9]*)\..*$/\1/g')
 if [[ "$major" -ge "15" ]]; then
   journalctl -u docker -n100 --no-pager
 else
   tail -100 /var/log/upstart/docker.log
 fi
+echo
+echo "Logout and login back!!!"
