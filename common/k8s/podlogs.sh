809c4c8a7ba9258d6f1511f8870a961747973707
Nestor Urquiza <nestor.urquiza@gmail.com>
2017-10-17 14:14:49 -0400

    Update podlogs.sh

--- common/k8s/podlogs.sh
+++ common/k8s/podlogs.sh
@@ -1,24 +1,59 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 # podlogs.sh
-# @description Tails all kubernetes logs for a given context
-#              Available contexts can be found by running 'kubectl config view'.  
+# @description Tails all kubernetes logs for a given context (kubectl config view ).  
 # @author Nestor Urquiza
 # @date 20171016
 
-# main
-USAGE="Usage: `basename $0` <context>"
-if [ $# -ne "1" ]; then
+# options
+while [[ $# -gt 1 ]]; do
+  key="$1"
+  case $key in
+    -c|--context)
+      context="$2"
+      shift
+    ;;
+    -r|--podNameRegex)
+      podNameRegex="$2"
+      shift
+    ;;
+    -l|--podLogPath)
+      podLogPath="$2"
+      shift
+    ;;
+    *)
+      # unknown
+    ;;
+  esac
+  shift
+done
+
+# functions
+function fail() {
+  echo $1
   echo $USAGE
   exit 1
+}
+
+# main
+USAGE="Usage:  `basename $0` (-c, --context) (from 'kubectl config view') [(-r, --podNameRegex) (defaults to all)] [(-l, --podLogPath) (defaults to pod stdout)]"
+
+if [ "$context" == "" ]; then
+  fail "Context is mandatory. Find available contexts by running 'kubectl config view'"
 fi
 
-context=$1
+if [ "$podNameRegex" == "" ]; then
+  podNameRegex=.
+fi
 
 kubectl config use-context $context
 command="cat <("
 for line in $(kubectl get pods | \
-  grep nodejs | grep Running | awk '{print $1}'); do 
-    command="$command (kubectl logs --tail=2 -f $line &) && "
-  done
+    grep "$podNameRegex" | grep Running | awk '{print $1}'); do 
+      if [ "$podLogPath" == "" ]; then
+        command="$command (kubectl logs --tail=2 -f $line &) && "
+      else
+        command="$command (kubectl exec -ti $line -- tail -2f $podLogPath &) && "
+      fi
+done
 command="$command echo)"
 eval $command
